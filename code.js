553a2a77b298b127e43a44fb48212d4d2b46204a
Ryan <106790344+RyanO-Projects@users.noreply.github.com>
2024-11-28 20:06:40 -0700

    Rough Draft
    Definitely isn't going to work.

--- code.js
+++ code.js
@@ -1,3 +1,36 @@
 function mergesort(array) {
+    // Base case, array is 0 or 1 in length, it is already sorted.
+    var len = array.length;
+    if ( len <= 1 ) {
+        return array;
+    }
+    
+    for ( var subLen = 1; subLen < len; subLen *= 2 ) { // Size of "subarrays" to sort for each iteration, doubles each time.
+        for ( var start = 0; start < (len-1); start += (2 * subLen) ) { // Iterates over pairs of "subarrays" for sorting.
+            var middle = start + subLen;
+            var end = start + (2 * subLen);
+
+            var start1 = start;
+            var mid = middle;
+
+            while ( start1 < end ) {
+                if (array[start1] <= array[mid]) {
+                    start1++;
+                } 
+                    
+                else {
+                    val = array[mid];
+                    for ( var i = mid; i > start1; i--) {
+                        array[i] = array[i - 1];
+                    }
+                    
+                    array[start1] = val;
+                    start1++;
+                    mid++;
+                }
+            }
+        }
+    }
+    
     return array;
 }
