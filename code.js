b11a79b895f5b77c735ff8b0c1540c196d726103
Ryan <106790344+RyanO-Projects@users.noreply.github.com>
2024-11-28 20:13:15 -0700

    Update code.js

--- code.js
+++ code.js
@@ -7,14 +7,12 @@ function mergesort(array) {
     
     for ( var subLen = 1; subLen < len; subLen *= 2 ) { // Size of "subarrays" to sort for each iteration, doubles each time.
         for ( var start = 0; start < (len-1); start += (2 * subLen) ) { // Iterates over pairs of "subarrays" for sorting.
+            var start1 = start;
             var middle = start + subLen;
             var end = start + (2 * subLen);
 
-            var start1 = start;
-            var mid = middle;
-
             while ( start1 < end ) {
-                if (array[start1] <= array[end]) {
+                if (array[start1] <= array[middle]) {
                     start1++;
                 } 
                     
@@ -26,7 +24,7 @@ function mergesort(array) {
                     
                     array[start1] = val;
                     start1++;
-                    mid++;
+                    //mid++;
                 }
             }
         }
